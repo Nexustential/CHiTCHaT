53d4d3d177a498f36bdb3faeba62748cb4eaf71f
Ryan Lewis <rclewis369@outlook.com>
2022-12-17 18:47:23 -0500

    finished layout and linked to Firebase

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB6iRA0VNoErvtF1u5vfylISwbKF1w66Lc",
+  authDomain: "chitchat-943e3.firebaseapp.com",
+  projectId: "chitchat-943e3",
+  storageBucket: "chitchat-943e3.appspot.com",
+  messagingSenderId: "951072838667",
+  appId: "1:951072838667:web:1558f12f6948c94299e30e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
